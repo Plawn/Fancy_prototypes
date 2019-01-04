825baf3061b09d2f9fcd5418dc7ece80c4500ca3
Plawn <plawn.yay@gmail.com>
2019-01-04 21:29:13 +0100

    first commit

---
+++ Fancy_prototypes.js
@@ -0,0 +1,47 @@
+//Object prototypes
+
+Object.prototype.forEach = function (func) { //func can take value and/or key
+    const t = this;
+    Object.keys(t).forEach(key => func(t[key], key));
+}
+Object.prototype.map = function (func) { //func can take value and/or key
+    const t = this;
+    Object.keys(t).forEach(key => t[key] = func(t[key], key));
+    return t;
+}
+
+Object.prototype.some = function (func) { //func can take value and/or key
+    const t = this;
+    Object.keys(t).some(key => func(t[key], key));
+}
+
+//String prototypes
+String.prototype.replaceAll = function(old, new_) {
+    const t = this;
+    return t.replace(new RegExp(old, 'g'), new_);
+};
+
+String.prototype.forEach = function (func) {
+    const t = this;
+    const n = t.length;
+    let i;
+    for (i = 0; i < n; i++) {
+        func(t[i], i);
+    }
+}
+
+String.prototype.map = function (func) {
+    const t = this;
+    let s = '';
+    t.forEach((elem, index) => s += func(elem, index));
+    return s;
+}
+
+String.prototype.some = function(func){
+    const t = this;
+    const n = t.length;
+    let i;
+    for (i = 0; i < n; i++) {
+        if (func(t[i], i)) return;
+    }
+}
